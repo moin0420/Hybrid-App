05b8bd86f89868ac5d6d821663a780b4a4bb76bb
moin0420 <khanmoin293@gmail.com>
2025-10-24 17:46:15 -0400

    Update 5 10/24

--- backend/app.js
+++ backend/app.js
@@ -160,9 +160,9 @@ app.put("/api/requisitions/:id", async (req, res) => {
 });
 
 // ===== SERVE FRONTEND =====
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
 // ===== START SERVER =====
